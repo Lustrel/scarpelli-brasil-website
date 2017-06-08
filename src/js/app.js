d7e26f342f83c2988c4ff99731f6f56b891b35bd
Stanley Sathler <stanleysathlerpinto@gmail.com>
2017-06-08 11:17:54 -0300

    (1) Mais coisas

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,20 @@
 {
 	"use strict";
 
-	(new ScarpelliBrasil.VideoPage());
-	(new ScarpelliBrasil.Pager());
+	var HomeVideo = (new ScarpelliBrasil.VideoPage());
+	var Pager = (new ScarpelliBrasil.Pager());
 
-	
+	/*
+
+	(function initialize()
+	{
+		if(isHomeVideoWatched()){
+			Pager.setInitialPageById("quem-somos");
+		} else {
+			Pager.setInitialPageById("video");
+			HomeVideo.start();
+		}
+	})();
+
+	*/
 })();
