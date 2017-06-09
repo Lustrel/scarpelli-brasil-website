50a6e11c78a17fab4c904af34da4489c6b162113
Stanley Sathler <stanleysathlerpinto@gmail.com>
2017-06-09 13:25:26 -0300

    .

--- src/js/app.js
+++ src/js/app.js
@@ -7,9 +7,28 @@
 	var Sidebar = (new ScarpelliBrasil.Sidebar("menu-icon", "side-menu"));
 
 	if( HomeVideo.isWatched() ){
-		Pager.setActiveSectionById("quem-somos");
+		var sectionId = sectionIdFromURL();
+
+		if( ! sectionId )
+			Pager.setActiveSectionById("quem-somos");
+		else
+			Pager.setActiveSectionById(sectionId);
 	} else {
 		Pager.setActiveSectionById("video");
 		HomeVideo.play();
+	}
+
+	function sectionIdFromURL()
+	{
+		var id = window.location.hash;
+
+		// Remove #
+		id = id.substring(1);
+
+		// Remove / at the end if exists
+		if( id[id.length - 1] == '/' )
+			id = id.substring(0, id.length - 1);
+
+		return id;
 	}
 })();
